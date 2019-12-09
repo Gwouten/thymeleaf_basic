700d344cfe94ec94ee1f254465db29f1abd5455f
mobapp15 <mobapp15@student.ahb.be>
2019-12-09 14:24:15 +0100

    Added controllers for index page and details page. Templates are working.

--- src/main/java/deloof/wouter/thymeleaf_basic/controllers/StudentController.java
+++ src/main/java/deloof/wouter/thymeleaf_basic/controllers/StudentController.java
@@ -1,33 +1,26 @@
 package deloof.wouter.thymeleaf_basic.controllers;
 
-import deloof.wouter.thymeleaf_basic.model.Student;
 import deloof.wouter.thymeleaf_basic.model.StudentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Optional;
-
 @Controller
 @RequestMapping( value = "/studenten")
-public class IndexController {
+public class StudentController {
 
     @Autowired
     StudentDAO dao;
 
-    // Find all students
-    @ModelAttribute( value = "all" )
-    public Iterable<Student> findAllStudents() {
-        return dao.findAll();
-    }
+    @RequestMapping( value = "/{studentId}", method = RequestMethod.GET)
+    public String student(ModelMap map, @PathVariable( name = "studentId") int studentId) {
 
-    @RequestMapping( value = {"", "/index"}, method = RequestMethod.GET)
-    public String index(ModelMap map) {
-        return "index";
+        map.addAttribute( "student", dao.findById( studentId ).get() );
+
+        return "student";
     }
 
 }
